68d16e91f9ca08032dddf23cce37ca40dd7b90d1
Tim Häring <tim.haering@goto.com>
2025-05-20 10:11:11 +0200

    docs: add examples

---
+++ examples/resources/github_connection_scopeconfig/import.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Copyright (c) HashiCorp, Inc.
+
+
+# github connection can be imported by specifying the numeric identifier of the connection and the scopeconfig.
+terraform import devlake_github_connection_scopeconfig.scopeconf "1,1"
